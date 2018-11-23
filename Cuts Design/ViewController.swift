61e149e2c005fc85a8601fe0a461015b6b9d5ba0
Christian Bach <christian.bach@live.dk>
2018-11-23 10:17:49 +0100

    Initial Commit

---
+++ Cuts Design/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cuts Design
+//
+//  Created by Christian Emil Bach on 23/11/2018.
+//  Copyright © 2018 Christian Emil Bach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
